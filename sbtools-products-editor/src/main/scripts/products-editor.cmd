29781c94fdea14d6c7c6cbbcb0078eebee1a88a6
Stefan Taferner <stefan.taferner@gmx.at>
2014-01-02 11:27:03 +0100

    FIX corrected start scripts

--- sbtools-products-editor/src/main/scripts/products-editor.cmd
+++ sbtools-products-editor/src/main/scripts/products-editor.cmd
@@ -3,7 +3,7 @@ setlocal
 
 cd libs
 set CLASSPATH=%CLASSPATH%;.
-java -jar sbtools-products-editor-0.2-SNAPSHOT.jar org.selfbus.sbtools.prodedit.ProdTool
+java -jar sbtools-products-editor-0.2-SNAPSHOT.jar org.selfbus.sbtools.prodedit.ProdEdit
 
 if %ERRORLEVEL% neq 0 (
    echo.
